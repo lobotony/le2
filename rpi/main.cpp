0699e0ff19c224d2220160d5d258838be85f4002
Tony Kostanjsek <lobotony666@yahoo.de>
2013-05-08 13:31:49 +0000

    moved esUtil contents over to main.cpp and removed the callbacks so lost::Engine is called directly

--- rpi/main.cpp
+++ rpi/main.cpp
@@ -15,24 +15,251 @@
 //    example is to demonstrate the basic concepts of 
 //    OpenGL ES 2.0 rendering.
 #include <stdlib.h>
-#include "esUtil.h"
 #include "lost/Engine.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/time.h>
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+
+#include  "bcm_host.h"
+#include "vc_dispmanx_types.h"
+
+#define ESUTIL_API
+#define ESCALLBACK
+
+#define ES_WINDOW_RGB           0
+#define ES_WINDOW_ALPHA         1 
+#define ES_WINDOW_DEPTH         2 
+#define ES_WINDOW_STENCIL       4
+#define ES_WINDOW_MULTISAMPLE   8
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+typedef struct
+{
+    GLfloat   m[4][4];
+} ESMatrix;
+
+typedef struct _escontext
+{
+
+  void*       userData; /// Put your user data here...
+  GLint       width;/// Window width
+  GLint       height;/// Window height
+  EGLNativeWindowType  hWnd; /// Window handle
+  EGLDisplay  eglDisplay;/// EGL display
+  EGLContext  eglContext;/// EGL context
+  EGLSurface  eglSurface;/// EGL surface
+
+} ESContext;
+
+
+void ESUTIL_API esInitContext ( ESContext *esContext );
+GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char *title, GLint width, GLint height, GLuint flags );
+void ESUTIL_API esMainLoop ( ESContext *esContext );
+
+EGLBoolean CreateEGLContext ( EGLNativeWindowType hWnd, EGLDisplay* eglDisplay,
+                              EGLContext* eglContext, EGLSurface* eglSurface,
+                              EGLint attribList[])
+{
+   EGLint numConfigs;
+   EGLint majorVersion;
+   EGLint minorVersion;
+   EGLDisplay display;
+   EGLContext context;
+   EGLSurface surface;
+   EGLConfig config;
+   EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
+   
+   display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+   if ( display == EGL_NO_DISPLAY )
+   {
+      return EGL_FALSE;
+   }
+
+   if ( !eglInitialize(display, &majorVersion, &minorVersion) )
+   {
+      return EGL_FALSE;
+   }
+
+
+   if ( !eglGetConfigs(display, NULL, 0, &numConfigs) )
+   {
+      return EGL_FALSE;
+   }
+
+   if ( !eglChooseConfig(display, attribList, &config, 1, &numConfigs) )
+   {
+      return EGL_FALSE;
+   }
+
+   surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)hWnd, NULL);
+   if ( surface == EGL_NO_SURFACE )
+   {
+      return EGL_FALSE;
+   }
+
+   context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs );
+   if ( context == EGL_NO_CONTEXT )
+   {
+      return EGL_FALSE;
+   }   
+   
+   if ( !eglMakeCurrent(display, surface, surface, context) )
+   {
+      return EGL_FALSE;
+   }
+   
+   *eglDisplay = display;
+   *eglSurface = surface;
+   *eglContext = context;
+   return EGL_TRUE;
+} 
 
 ///
-// Initialize the shader and program object
+//  WinCreate() - RaspberryPi, direct surface (No X, Xlib)
 //
-int Init ( ESContext *esContext )
+//      This function initialized the display and window for EGL
+//
+EGLBoolean WinCreate(ESContext *esContext, const char *title) 
 {
-   lost::Engine::instance()->doStartup();
+   int32_t success = 0;
+
+   static EGL_DISPMANX_WINDOW_T nativewindow;
+
+   DISPMANX_ELEMENT_HANDLE_T dispman_element;
+   DISPMANX_DISPLAY_HANDLE_T dispman_display;
+   DISPMANX_UPDATE_HANDLE_T dispman_update;
+   VC_RECT_T dst_rect;
+   VC_RECT_T src_rect;
+   
+
+   uint32_t display_width;
+   uint32_t display_height;
+
+   // create an EGL window surface, passing context width/height
+   success = graphics_get_display_size(0 /* LCD */, &display_width, &display_height);
+   if ( success < 0 )
+   {
+      return EGL_FALSE;
+   }
+
+
+   VC_DISPMANX_ALPHA_T alpha = { DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,255,0 };
+   
+   dst_rect.x = 0;
+   dst_rect.y = 0;
+   dst_rect.width = display_width;
+   dst_rect.height = display_height;
+      
+   src_rect.x = 0;
+   src_rect.y = 0;
+   src_rect.width = display_width << 16;
+   src_rect.height = display_height << 16;   
+
+   dispman_display = vc_dispmanx_display_open( 0 /* LCD */);
+   dispman_update = vc_dispmanx_update_start( 0 );
+         
+   dispman_element = vc_dispmanx_element_add ( dispman_update, dispman_display,
+      0/*layer*/, &dst_rect, 0/*src*/,
+      &src_rect, DISPMANX_PROTECTION_NONE, &alpha, 0/*clamp*/, (DISPMANX_TRANSFORM_T)0/*transform*/);
+      
+   nativewindow.element = dispman_element;
+   nativewindow.width = display_width;
+   nativewindow.height = display_height;
+   vc_dispmanx_update_submit_sync( dispman_update );
+   
+   esContext->hWnd = &nativewindow;
+
+  return EGL_TRUE;
+}
+
+void ESUTIL_API esInitContext ( ESContext *esContext )
+{
+   bcm_host_init();
+   if ( esContext != NULL )
+   {
+      memset( esContext, 0, sizeof( ESContext) );
+   }
+}
+
+GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char* title, GLint width, GLint height, GLuint flags )
+{
+   EGLint attribList[] =
+   {
+       EGL_RED_SIZE,       8,
+       EGL_GREEN_SIZE,     8,
+       EGL_BLUE_SIZE,      8,
+       EGL_ALPHA_SIZE,     (flags & ES_WINDOW_ALPHA) ? 8 : EGL_DONT_CARE,
+       EGL_DEPTH_SIZE,     (flags & ES_WINDOW_DEPTH) ? 8 : EGL_DONT_CARE,
+       EGL_STENCIL_SIZE,   (flags & ES_WINDOW_STENCIL) ? 8 : EGL_DONT_CARE,
+       EGL_SAMPLE_BUFFERS, (flags & ES_WINDOW_MULTISAMPLE) ? 1 : 0,
+       EGL_NONE
+   };
+   
+   if ( esContext == NULL )
+   {
+      return GL_FALSE;
+   }
+
+   esContext->width = width;
+   esContext->height = height;
+
+   if ( !WinCreate ( esContext, title) )
+   {
+      return GL_FALSE;
+   }
+
+  
+   if ( !CreateEGLContext ( esContext->hWnd,
+                            &esContext->eglDisplay,
+                            &esContext->eglContext,
+                            &esContext->eglSurface,
+                            attribList) )
+   {
+      return GL_FALSE;
+   }
+   
+
    return GL_TRUE;
 }
 
-///
-// Draw a triangle using the shader pair created in Init()
-//
-void Draw ( ESContext *esContext )
+void ESUTIL_API esMainLoop ( ESContext *esContext )
 {
-   lost::Engine::instance()->doUpdate();
+    struct timeval t1, t2;
+    struct timezone tz;
+    float deltatime;
+    float totaltime = 0.0f;
+    unsigned int frames = 0;
+
+    gettimeofday ( &t1 , &tz );
+
+    while(true)
+    {
+        gettimeofday(&t2, &tz);
+        deltatime = (float)(t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) * 1e-6);
+        t1 = t2;
+
+         lost::Engine::instance()->doUpdate();
+
+        eglSwapBuffers(esContext->eglDisplay, esContext->eglSurface);
+
+        totaltime += deltatime;
+        frames++;
+        if (totaltime >  2.0f)
+        {
+            printf("%4d frames rendered in %1.4f seconds -> FPS=%3.4f\n", frames, totaltime, frames/totaltime);
+            totaltime -= 2.0f;
+            frames = 0;
+        }
+    }
 }
 
 int main ( int argc, char *argv[] )
@@ -44,10 +271,7 @@ int main ( int argc, char *argv[] )
 
    esCreateWindow ( &esContext, "Hello Triangle", 1280, 800, ES_WINDOW_RGB | ES_WINDOW_ALPHA );
 
-   if ( !Init ( &esContext ) )
-      return 0;
-
-   esRegisterDrawFunc ( &esContext, Draw );
+   lost::Engine::instance()->doStartup();
 
    esMainLoop ( &esContext );
 }
