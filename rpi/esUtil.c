a68e49c9b9b104b02838353f9370b16424ddc476
Tony Kostanjsek <lobotony666@yahoo.de>
2013-05-08 13:03:08 +0000

    started work on reducing RPi driver code for le2: remove disabled code, comments and obsolete declarations

--- rpi/esUtil.c
+++ rpi/esUtil.c
@@ -1,23 +1,3 @@
-//
-// Book:      OpenGL(R) ES 2.0 Programming Guide
-// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
-// ISBN-10:   0321502795
-// ISBN-13:   9780321502797
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780321563835
-//            http://www.opengles-book.com
-//
-
-// ESUtil.c
-//
-//    A utility library for OpenGL ES.  This library provides a
-//    basic common framework for the example applications in the
-//    OpenGL ES 2.0 Programming Guide.
-//
-
-///
-//  Includes
-//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,11 +9,7 @@
 
 #include  "bcm_host.h"
 #include "vc_dispmanx_types.h"
-///
-// CreateEGLContext()
-//
-//    Creates an EGL rendering context and all associated elements
-//
+
 EGLBoolean CreateEGLContext ( EGLNativeWindowType hWnd, EGLDisplay* eglDisplay,
                               EGLContext* eglContext, EGLSurface* eglSurface,
                               EGLint attribList[])
@@ -47,47 +23,40 @@ EGLBoolean CreateEGLContext ( EGLNativeWindowType hWnd, EGLDisplay* eglDisplay,
    EGLConfig config;
    EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
    
-   
-   // Get Display
    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
    if ( display == EGL_NO_DISPLAY )
    {
       return EGL_FALSE;
    }
 
-   // Initialize EGL
    if ( !eglInitialize(display, &majorVersion, &minorVersion) )
    {
       return EGL_FALSE;
    }
 
-   // Get configs
+
    if ( !eglGetConfigs(display, NULL, 0, &numConfigs) )
    {
       return EGL_FALSE;
    }
 
-   // Choose config
    if ( !eglChooseConfig(display, attribList, &config, 1, &numConfigs) )
    {
       return EGL_FALSE;
    }
 
-   // Create a surface
    surface = eglCreateWindowSurface(display, config, (EGLNativeWindowType)hWnd, NULL);
    if ( surface == EGL_NO_SURFACE )
    {
       return EGL_FALSE;
    }
 
-   // Create a GL context
    context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs );
    if ( context == EGL_NO_CONTEXT )
    {
       return EGL_FALSE;
    }   
    
-   // Make the context current
    if ( !eglMakeCurrent(display, surface, surface, context) )
    {
       return EGL_FALSE;
@@ -128,10 +97,7 @@ EGLBoolean WinCreate(ESContext *esContext, const char *title)
    }
 
 
-    VC_DISPMANX_ALPHA_T alpha; //= { DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,0,0 };
-    alpha.flags = DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS;
-    alpha.opacity = 255;
-    alpha.mask = NULL;
+   VC_DISPMANX_ALPHA_T alpha = { DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,255,0 };
    
    dst_rect.x = 0;
    dst_rect.y = 0;
@@ -157,17 +123,12 @@ EGLBoolean WinCreate(ESContext *esContext, const char *title)
    
    esContext->hWnd = &nativewindow;
 
-	return EGL_TRUE;
+  return EGL_TRUE;
 }
-///
-//  userInterrupt()
-//
-//      Reads from X11 event loop and interrupt program if there is a keypress, or
-//      window close action.
-//
+
 GLboolean userInterrupt(ESContext *esContext)
 {
-	//GLboolean userinterrupt = GL_FALSE;
+  //GLboolean userinterrupt = GL_FALSE;
     //return userinterrupt;
     
     // Ctrl-C for now to stop
@@ -175,18 +136,6 @@ GLboolean userInterrupt(ESContext *esContext)
     return GL_FALSE;
 }
 
-//////////////////////////////////////////////////////////////////
-//
-//  Public Functions
-//
-//
-
-///
-//  esInitContext()
-//
-//      Initialize ES utility context.  This must be called before calling any other
-//      functions.
-//
 void ESUTIL_API esInitContext ( ESContext *esContext )
 {
    bcm_host_init();
@@ -196,19 +145,6 @@ void ESUTIL_API esInitContext ( ESContext *esContext )
    }
 }
 
-
-///
-//  esCreateWindow()
-//
-//      title - name for title bar of window
-//      width - width of window to create
-//      height - height of window to create
-//      flags  - bitwise or of window creation flags 
-//          ES_WINDOW_ALPHA       - specifies that the framebuffer should have alpha
-//          ES_WINDOW_DEPTH       - specifies that a depth buffer should be created
-//          ES_WINDOW_STENCIL     - specifies that a stencil buffer should be created
-//          ES_WINDOW_MULTISAMPLE - specifies that a multi-sample buffer should be created
-//
 GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char* title, GLint width, GLint height, GLuint flags )
 {
    EGLint attribList[] =
@@ -250,13 +186,6 @@ GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char* title, G
    return GL_TRUE;
 }
 
-
-///
-//  esMainLoop()
-//
-//    Start the main loop for the OpenGL ES application
-//
-
 void ESUTIL_API esMainLoop ( ESContext *esContext )
 {
     struct timeval t1, t2;
@@ -291,49 +220,18 @@ void ESUTIL_API esMainLoop ( ESContext *esContext )
     }
 }
 
-
-///
-//  esRegisterDrawFunc()
-//
 void ESUTIL_API esRegisterDrawFunc ( ESContext *esContext, void (ESCALLBACK *drawFunc) (ESContext* ) )
 {
    esContext->drawFunc = drawFunc;
 }
 
-
-///
-//  esRegisterUpdateFunc()
-//
 void ESUTIL_API esRegisterUpdateFunc ( ESContext *esContext, void (ESCALLBACK *updateFunc) ( ESContext*, float ) )
 {
    esContext->updateFunc = updateFunc;
 }
 
-
-///
-//  esRegisterKeyFunc()
-//
 void ESUTIL_API esRegisterKeyFunc ( ESContext *esContext,
                                     void (ESCALLBACK *keyFunc) (ESContext*, unsigned char, int, int ) )
 {
    esContext->keyFunc = keyFunc;
-}
-
-
-///
-// esLogMessage()
-//
-//    Log an error message to the debug output for the platform
-//
-void ESUTIL_API esLogMessage ( const char *formatStr, ... )
-{
-    va_list params;
-    char buf[BUFSIZ];
-
-    va_start ( params, formatStr );
-    vsprintf ( buf, formatStr, params );
-    
-    printf ( "%s", buf );
-    
-    va_end ( params );
 }
--- rpi/esUtil.h
+++ rpi/esUtil.h
@@ -1,56 +1,21 @@
-//
-// Book:      OpenGL(R) ES 2.0 Programming Guide
-// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
-// ISBN-10:   0321502795
-// ISBN-13:   9780321502797
-// Publisher: Addison-Wesley Professional
-// URLs:      http://safari.informit.com/9780321563835
-//            http://www.opengles-book.com
-//
-
-//
-/// \file ESUtil.h
-/// \brief A utility library for OpenGL ES.  This library provides a
-///        basic common framework for the example applications in the
-///        OpenGL ES 2.0 Programming Guide.
-//
 #ifndef ESUTIL_H
 #define ESUTIL_H
 
-///
-//  Includes
-//
 #include <GLES2/gl2.h>
 #include <EGL/egl.h>
 
 #ifdef __cplusplus
 
 extern "C" {
-#endif
-
-   
-///
-//  Macros
-//
+#endif   
 #define ESUTIL_API
 #define ESCALLBACK
 
-
-/// esCreateWindow flag - RGB color buffer
 #define ES_WINDOW_RGB           0
-/// esCreateWindow flag - ALPHA color buffer
 #define ES_WINDOW_ALPHA         1 
-/// esCreateWindow flag - depth buffer
 #define ES_WINDOW_DEPTH         2 
-/// esCreateWindow flag - stencil buffer
 #define ES_WINDOW_STENCIL       4
-/// esCreateWindow flat - multi-sample buffer
 #define ES_WINDOW_MULTISAMPLE   8
-
-
-///
-// Types
-//
 
 #ifndef FALSE
 #define FALSE 0
@@ -66,155 +31,26 @@ typedef struct
 
 typedef struct _escontext
 {
-   /// Put your user data here...
-   void*       userData;
 
-   /// Window width
-   GLint       width;
-
-   /// Window height
-   GLint       height;
-
-   /// Window handle
-   EGLNativeWindowType  hWnd;
-
-   /// EGL display
-   EGLDisplay  eglDisplay;
-      
-   /// EGL context
-   EGLContext  eglContext;
-
-   /// EGL surface
-   EGLSurface  eglSurface;
-
-   /// Callbacks
-   void (ESCALLBACK *drawFunc) ( struct _escontext * );
-   void (ESCALLBACK *keyFunc) ( struct _escontext *, unsigned char, int, int );
-   void (ESCALLBACK *updateFunc) ( struct _escontext *, float deltaTime );
+  void*       userData; /// Put your user data here...
+  GLint       width;/// Window width
+  GLint       height;/// Window height
+  EGLNativeWindowType  hWnd; /// Window handle
+  EGLDisplay  eglDisplay;/// EGL display
+  EGLContext  eglContext;/// EGL context
+  EGLSurface  eglSurface;/// EGL surface
+  void (ESCALLBACK *drawFunc) ( struct _escontext * );
+  void (ESCALLBACK *keyFunc) ( struct _escontext *, unsigned char, int, int );
+  void (ESCALLBACK *updateFunc) ( struct _escontext *, float deltaTime );
 } ESContext;
 
 
-///
-//  Public Functions
-//
-
-//
-///
-/// \brief Initialize ES framework context.  This must be called before calling any other functions.
-/// \param esContext Application context
-//
 void ESUTIL_API esInitContext ( ESContext *esContext );
-
-//
-/// \brief Create a window with the specified parameters
-/// \param esContext Application context
-/// \param title Name for title bar of window
-/// \param width Width in pixels of window to create
-/// \param height Height in pixels of window to create
-/// \param flags Bitfield for the window creation flags 
-///         ES_WINDOW_RGB     - specifies that the color buffer should have R,G,B channels
-///         ES_WINDOW_ALPHA   - specifies that the color buffer should have alpha
-///         ES_WINDOW_DEPTH   - specifies that a depth buffer should be created
-///         ES_WINDOW_STENCIL - specifies that a stencil buffer should be created
-///         ES_WINDOW_MULTISAMPLE - specifies that a multi-sample buffer should be created
-/// \return GL_TRUE if window creation is succesful, GL_FALSE otherwise
 GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char *title, GLint width, GLint height, GLuint flags );
-
-//
-/// \brief Start the main loop for the OpenGL ES application
-/// \param esContext Application context
-//
 void ESUTIL_API esMainLoop ( ESContext *esContext );
-
-//
-/// \brief Register a draw callback function to be used to render each frame
-/// \param esContext Application context
-/// \param drawFunc Draw callback function that will be used to render the scene
-//
 void ESUTIL_API esRegisterDrawFunc ( ESContext *esContext, void (ESCALLBACK *drawFunc) ( ESContext* ) );
-
-//
-/// \brief Register an update callback function to be used to update on each time step
-/// \param esContext Application context
-/// \param updateFunc Update callback function that will be used to render the scene
-//
 void ESUTIL_API esRegisterUpdateFunc ( ESContext *esContext, void (ESCALLBACK *updateFunc) ( ESContext*, float ) );
-
-//
-/// \brief Register an keyboard input processing callback function
-/// \param esContext Application context
-/// \param keyFunc Key callback function for application processing of keyboard input
-//
-void ESUTIL_API esRegisterKeyFunc ( ESContext *esContext, 
-                                    void (ESCALLBACK *drawFunc) ( ESContext*, unsigned char, int, int ) );
-//
-/// \brief Log a message to the debug output for the platform
-/// \param formatStr Format string for error log.  
-//
-void ESUTIL_API esLogMessage ( const char *formatStr, ... );
-
-//
-/// \brief multiply matrix specified by result with a scaling matrix and return new matrix in result
-/// \param result Specifies the input matrix.  Scaled matrix is returned in result.
-/// \param sx, sy, sz Scale factors along the x, y and z axes respectively
-//
-void ESUTIL_API esScale(ESMatrix *result, GLfloat sx, GLfloat sy, GLfloat sz);
-
-//
-/// \brief multiply matrix specified by result with a translation matrix and return new matrix in result
-/// \param result Specifies the input matrix.  Translated matrix is returned in result.
-/// \param tx, ty, tz Scale factors along the x, y and z axes respectively
-//
-void ESUTIL_API esTranslate(ESMatrix *result, GLfloat tx, GLfloat ty, GLfloat tz);
-
-//
-/// \brief multiply matrix specified by result with a rotation matrix and return new matrix in result
-/// \param result Specifies the input matrix.  Rotated matrix is returned in result.
-/// \param angle Specifies the angle of rotation, in degrees.
-/// \param x, y, z Specify the x, y and z coordinates of a vector, respectively
-//
-void ESUTIL_API esRotate(ESMatrix *result, GLfloat angle, GLfloat x, GLfloat y, GLfloat z);
-
-//
-// \brief multiply matrix specified by result with a perspective matrix and return new matrix in result
-/// \param result Specifies the input matrix.  new matrix is returned in result.
-/// \param left, right Coordinates for the left and right vertical clipping planes
-/// \param bottom, top Coordinates for the bottom and top horizontal clipping planes
-/// \param nearZ, farZ Distances to the near and far depth clipping planes.  Both distances must be positive.
-//
-void ESUTIL_API esFrustum(ESMatrix *result, float left, float right, float bottom, float top, float nearZ, float farZ);
-
-//
-/// \brief multiply matrix specified by result with a perspective matrix and return new matrix in result
-/// \param result Specifies the input matrix.  new matrix is returned in result.
-/// \param fovy Field of view y angle in degrees
-/// \param aspect Aspect ratio of screen
-/// \param nearZ Near plane distance
-/// \param farZ Far plane distance
-//
-void ESUTIL_API esPerspective(ESMatrix *result, float fovy, float aspect, float nearZ, float farZ);
-
-//
-/// \brief multiply matrix specified by result with a perspective matrix and return new matrix in result
-/// \param result Specifies the input matrix.  new matrix is returned in result.
-/// \param left, right Coordinates for the left and right vertical clipping planes
-/// \param bottom, top Coordinates for the bottom and top horizontal clipping planes
-/// \param nearZ, farZ Distances to the near and far depth clipping planes.  These values are negative if plane is behind the viewer
-//
-void ESUTIL_API esOrtho(ESMatrix *result, float left, float right, float bottom, float top, float nearZ, float farZ);
-
-//
-/// \brief perform the following operation - result matrix = srcA matrix * srcB matrix
-/// \param result Returns multiplied matrix
-/// \param srcA, srcB Input matrices to be multiplied
-//
-void ESUTIL_API esMatrixMultiply(ESMatrix *result, ESMatrix *srcA, ESMatrix *srcB);
-
-//
-//// \brief return an indentity matrix 
-//// \param result returns identity matrix
-//
-void ESUTIL_API esMatrixLoadIdentity(ESMatrix *result);
+void ESUTIL_API esRegisterKeyFunc ( ESContext *esContext, void (ESCALLBACK *drawFunc) ( ESContext*, unsigned char, int, int ) );
 
 #ifdef __cplusplus
 }
