ec2cfaf9a8b5fb19afeb329c2f6ec433dfc8869b
Tony Kostanjsek <lobotony666@yahoo.de>
2013-06-13 18:39:42 +0200

    started work on z updates, but went a simpler route

--- guiro/UpdateSystem.h
+++ guiro/UpdateSystem.h
@@ -1,12 +1,16 @@
 #ifndef GUIRO_UPDATESYSTEM_H
 #define GUIRO_UPDATESYSTEM_H
 
+#include "guiro/types.h"
+
 namespace lost
 {
 struct UpdateSystem
 {
   UpdateSystem();
   ~UpdateSystem();
+  
+  void update(); // performs the actual updates in the correct order
 };
 }
 
