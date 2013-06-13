1c917a740692cee6babd3d173da2241ae8c27f22
Tony Kostanjsek <lobotony666@yahoo.de>
2013-06-13 18:37:35 +0200

    fixed typo

--- lost/Clock.h
+++ lost/Clock.h
@@ -10,7 +10,7 @@ struct Clock
   void reset(); // resets startTime and lastUpdateTime to now.
   void update();
   
-  double startTime; // absolute time value in seconds with which thid clock was started and initialised
+  double startTime; // absolute time value in seconds with which this clock was started and initialised
   double lastUpdateTime;   // time of last update call
 
   double deltaUpdate; // delta since last update call
