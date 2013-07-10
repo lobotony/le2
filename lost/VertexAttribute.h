f8f6471a646d90ad68a09337f0718792e501907f
Tony Kostanjsek <lobotony666@yahoo.de>
2013-07-10 18:28:05 +0200

    fixed #8

--- lost/VertexAttribute.h
+++ lost/VertexAttribute.h
@@ -28,15 +28,18 @@ struct VertexAttribute
   GLint           size;       // size of array
   GLint           location;
 
-  VertexAttribute();
-  VertexAttribute(const string& inName,
-          GLint inIndex,
-          GLenum inGlType,
-          GLint inSize,
-          GLint loc);
+  VertexAttribute()
+  {
+    index = 0;
+    glType = 0;
+    size = 0;
+    location = 0;
+  }
 
-  ~VertexAttribute();
-  
+  VertexAttribute(const string& inName, GLint inIndex, GLenum inGlType, GLint inSize, GLint loc)
+  : name(inName), index(inIndex), glType(inGlType), size(inSize), location(loc)
+  {
+  }  
   
 };
 
