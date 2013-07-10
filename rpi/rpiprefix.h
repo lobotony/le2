82db5b8db467a5f0f954d505b77c6027ace7705e
Tony Kostanjsek <lobotony666@yahoo.de>
2013-07-10 15:15:21 +0000

    sped up rpi build by using precompiled headers

---
+++ rpi/rpiprefix.h
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+
+#include "lost/prefix-shared.h"
+#include <iostream>
+#include <string>
+#include "json/json.h"
+#include <algorithm>
+#include <cmath>
+#include "lost/Vec2.h"
+#include "lost/Vec3.h"
+#include "lost/Vec4.h"
+#include "lost/Matrix.h"
+#include "lost/Rect.h"
+
+#endif
