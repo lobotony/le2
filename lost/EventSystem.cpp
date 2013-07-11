981637046e0925365964b0737da80dbf84495acb
Tony Kostanjsek <lobotony666@yahoo.de>
2013-07-11 12:25:20 +0200

    started to port lostengine/guiro/EventManager

--- lost/EventSystem.cpp
+++ lost/EventSystem.cpp
@@ -1,4 +1,5 @@
 #include "lost/EventSystem.h"
+#include "lost/views/View.h"
 
 namespace lost
 {
@@ -10,4 +11,33 @@ EventSystem::~EventSystem()
 {
 }
 
+vector<View*> EventSystem::findViewStack(Event* event)
+{
+  vector<View*> viewStack;
+  
+  Vec2 pos(event->mouseEvent.x, event->mouseEvent.y);
+  bool containsPoint = rootView->containsPoint(pos);
+  View* view = rootView;
+  
+  while(containsPoint)
+  {
+    viewStack.push_back(view);
+    containsPoint = false;
+
+    for(auto i=view->subviews.rbegin(); i!=view->subviews.rend(); ++i)
+    {
+      View* v = (*i).get();
+      if(v->isVisibleWithinSuperviews() && v->containsPoint(pos))
+      {
+        view = v;
+        containsPoint = true;
+        break;
+      }
+    }
+  }
+  
+  return viewStack;
 }
+
+}
+
--- lost/EventSystem.h
+++ lost/EventSystem.h
@@ -1,12 +1,25 @@
 #ifndef LOST_EVENTYSTEM_H
 #define LOST_EVENTYSTEM_H
 
+#include "lost/Event.h"
+
 namespace lost
 {
+
 struct EventSystem
 {
   EventSystem();
   ~EventSystem();
+  
+  
+  vector<View*> findViewStack(Event* mouseEvent);
+  
+  View* rootView;
+  vector<View*> previousMouseMoveStack;
+  vector<View*> previousMouseClickStack;
+  vector<View*> previousFocusStack;
+  bool focusChanged;
+  View* currentlyFocusedView;
 };
 }
 
