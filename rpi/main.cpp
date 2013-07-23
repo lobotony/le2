cec69449b3f821239d8ab433e34c47d4f0e658db
Tony Kostanjsek <lobotony666@yahoo.de>
2013-07-23 20:19:24 +0000

    tried AnimTest on Rpi

--- rpi/main.cpp
+++ rpi/main.cpp
@@ -2,11 +2,15 @@
 #include "apps/DemoApp.h"
 #include "apps/SunApp.h"
 #include "apps/UiTestApp.h"
+#include "apps/BezierEd.h"
+#include "apps/AnimTest.h"
 
 int main(int argn, char** argc)
 {
 //  lost::run(new lost::DemoApp);
 //  lost::run(new lost::SunApp);
-  lost::run(new lost::UiTestApp);
+//  lost::run(new lost::UiTestApp);
+//  lost::run(new lost::BezierEd);
+  lost::run(new lost::AnimTest);
   return EXIT_SUCCESS;
 }
