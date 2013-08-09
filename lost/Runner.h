58a99792cb72f0555c41afdd32cb2f601161dea4
Tony Kostanjsek <lobotony666@yahoo.de>
2013-08-09 17:18:59 +0200

    fixed comments

--- lost/Runner.h
+++ lost/Runner.h
@@ -7,17 +7,18 @@ namespace lost
 
 struct Application;
 
-/** This is basically a cross plattform main() method that either creates a demo app to run
- * if called with no parameters, or uses the provided app to startup a new application.
+/** This is basically a cross plattform main() method that starts the provided application instance.
  * 
  * Implementations of the functions below must be provided at link time, i.e. you have to add the
  * correct platform-specific files to your project or makefile.
  *
- * TODO: pass in command line arguments and probably a configuration file as well to tweak 
- *       aspects of OpenGL context
+ * Since the application can read an optional json configuration file, the runner can use the 
+ * applications config file to e.g. configure the OpenGL context. 
+ *
+ * TODO: pass in command line arguments
  */
 
-void run(Application* app); // uses provided app. Subclass Application and override as required.
+void run(Application* app); 
 
 }
 
