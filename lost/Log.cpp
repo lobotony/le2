6b6f77924d6df45c56b44d38159f8de02f02a69e
Tony Kostanjsek <lobotony666@yahoo.de>
2013-10-04 13:22:43 +0200

    added syslog

--- lost/Log.cpp
+++ lost/Log.cpp
@@ -1,12 +1,20 @@
 #include "lost/Log.h"
 #include <iostream>
 
+#ifdef LOST_PLATFORM_RPI
+#include <syslog.h>
+#endif
+
+
 namespace lost
 {
 
 void log(const string& msg)
 {
   std::cout << msg.c_str() << std::endl;
+#ifdef LOST_PLATFORM_RPI
+  syslog(LOG_MAKEPRI(LOG_USER, LOG_DEBUG), msg.c_str());
+#endif
 }
 
 }
