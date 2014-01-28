80900a29d2ea51ba1956d852d052d3a5e6ea5335
Tony Kostanjsek <lobotony666@yahoo.de>
2014-01-28 22:31:20 +0100

    fixed build

--- mac/main.mm
+++ mac/main.mm
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
 //  lost::run(new lost::DemoApp);
 //  lost::run(new lost::SunApp);
 //  lost::run(new lost::UiTestApp);
-  lost::run(new lost::RpiDemo);
+//  lost::run(new lost::RpiDemo);
 //  lost::run(new lost::BezierEd);
 
-//  lost::run(new lost::AnimTest);
+  lost::run(new lost::AnimTest);
 
   return EXIT_SUCCESS;
 }
